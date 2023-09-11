595e4c6ed2ea294d6d2b3dc23e0ae09c15b135b8
BeatLink <47278150+BeatLink@users.noreply.github.com>
2023-09-11 16:18:07 +0000

    Add update agenda 2

---
+++ Agenda/updateAgenda2.js
@@ -0,0 +1,92 @@
+/*
+    Instructions: Paste the below into a new JS Backend Script note. Set the note variables to the IDs of the notes you want to use for your categories.
+    Remember to set #run=hourly as a label for this note.
+    To configure date formats use https://day.js.org/docs/en/display/format
+*/
+
+// Imports -----------------------------------------------------------------------------
+var isBetween = require('dayjs/plugin/isBetween')
+api.dayjs.extend(isBetween)
+
+var advancedFormat = require('dayjs/plugin/advancedFormat')
+api.dayjs.extend(advancedFormat)
+
+var isSameOrBefore = require('dayjs/plugin/isSameOrBefore')
+api.dayjs.extend(isSameOrBefore)
+
+
+// Criteria Variables --------------------------------------------------------------------
+let useNumberOfDays = true // intervals are determined based on number of days
+let now = api.dayjs()
+let startOfToday = now.startOf("day")
+let endOfToday = now.endOf("day")
+let endOfThisWeek = useNumberOfDays ? startOfToday.add(7, "day") : now.endOf("week")
+let endOfThisMonth = useNumberOfDays ? startOfToday.add(30, "day") : now.endOf("month")
+let endOfThisYear = useNumberOfDays ? startOfToday.add(365, "day") : now.endOf("year")
+
+// User Set Variables -----------------------------------------------------------------
+let nowNote = "QpWUSNl5ompI"
+let upcomingNote = "Qcf6WxdKgvv8"
+let templateId = "Yps2a0UwNkHK"
+let dueDatetimeLabel = "due"
+let intervals = {
+    'now': {
+        'criteria': (datetime) => {return datetime.isBefore(now)},
+        'parentNote': nowNote,
+        'formatString': "HH:mm"
+    },
+    'restOfDay': {
+        'criteria': (datetime) => {return datetime.isBetween(now, endOfToday, null, '[)')},
+        'parentNote': upcomingNote,
+        'formatString': "HH:mm"
+    },
+    'thisWeek': {
+        'criteria': (datetime) => {return datetime.isBetween(endOfToday, endOfThisWeek, null, '[)')},
+        'parentNote': upcomingNote,
+        'formatString': "ddd"
+    },
+    'thisMonth': {
+        'criteria': (datetime) => {return datetime.isBetween(endOfThisWeek, endOfThisMonth, null, '[)')},
+        'parentNote': upcomingNote,
+        'formatString': "Do"
+    },
+    'thisYear': {
+        'criteria': (datetime) => {return datetime.isBetween(endOfThisMonth, endOfThisYear, null, '[]')},
+        'parentNote': upcomingNote,
+        'formatString': "MMMM"
+    },
+    'future': {
+        'criteria':  (datetime) => {return datetime.isAfter(endOfThisYear)},
+        'parentNote': upcomingNote,
+        'formatString': "YYYY"
+    }
+} 
+
+
+
+function run_script() {
+    // Clear existing note branches
+    for (let note of api.searchForNotes(`note.parents.noteId=${nowNote} OR note.parents.noteId=${upcomingNote}`)){
+        api.toggleNoteInParent(false, note.noteId, nowNote)
+        api.toggleNoteInParent(false, note.noteId, upcomingNote)    
+    }
+        
+    // Set notes according to criteria
+    for (let note of api.searchForNotes(`# ~template.noteId=${templateId} AND not(note.parents.relations.template.noteId=${templateId})`)){ 
+        if (note.getLabelValue(dueDatetimeLabel)){
+            let dueDatetime = api.dayjs(note.getLabelValue(dueDatetimeLabel))       
+            for (let interval in intervals){
+                var criteria = intervals[interval]['criteria']
+                var parentNote = intervals[interval]['parentNote']
+                var formatString = intervals[interval]['formatString']
+                var datetimeFormatted = dueDatetime.format(formatString)
+                if (criteria(dueDatetime)){
+                    api.toggleNoteInParent(criteria, note.noteId, parentNote, datetimeFormatted)
+                    continue
+                }        
+            }
+        }
+    }
+}
+
+run_script()
