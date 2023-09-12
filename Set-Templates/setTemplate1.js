475998f88164dd58fde0c9dd81f45d79eddbad89
BeatLink <47278150+BeatLink@users.noreply.github.com>
2023-09-12 00:51:14 +0000

    Delete set template script

--- Set-Templates/setTemplate1.js
+++
@@ -1,6 +0,0 @@
-async function run_script() {
-    await api.runOnBackend((currentNoteID) => {
-        api.getNote(currentNoteID).setRelation("template", "<path-to-template-1>")         
-    }, [await api.getActiveContextNote().noteId]);
-}
-run_script()
