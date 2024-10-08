117a18c1fd4d33d03fa50be0a144ee8ca40897e8
BeatLink <beatlink+git@simplelogin.co>
2024-10-07 23:17:42 -0500

    Update My Day / Starred

---
+++ Scripts/Starred/setupButton.js
@@ -0,0 +1,24 @@
+/*
+    Instructions: 
+    Paste the below into a new JS Frontend Script note. 
+    Create a relation for ~toggleStarredScript pointing to the toggleStarred.js
+    Add the following label: #run=frontendStartup
+*/
+
+async function setupButton() {
+    let toggleStarredNote = await api.currentNote.getRelationValue("toggleStarredScript")
+    await api.runOnBackend(
+        (toggleStarredNote) => {
+            api.createOrUpdateLauncher({
+                id: "toggleStarredButton",
+                title: "Toggle Starred",
+                icon: "bxs-star-half",
+                type: "script",
+                isVisible: true,
+                scriptNoteId: toggleStarredNote
+            });
+        }, 
+        [toggleStarredNote]);
+}
+
+setupButton()
