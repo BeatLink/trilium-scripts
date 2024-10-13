fbcf5175960164e26327982c2cabe521188d41a0
BeatLink <beatlink+git@simplelogin.co>
2024-10-13 11:35:45 -0500

    Update Recurrence

---
+++ Recurrence/README.md
@@ -0,0 +1,34 @@
+# Recurrence
+
+# trillium-recurrence
+Scripts for repeating/recurring tasks
+
+Script to update repeating to-dos
+
+## To Use
+1. Create a Note template for repeating tasks. This note must have a date time label for due dates to be updated. 
+2. Create a JS Frontend script and copy the contents of [recurrencelib.js](recurrencelib.js)
+3. Create a JS Frontend script and copy the contents of [recurrenceWidget.js](./recurrenceWidget.js)
+   1. Set a label of #widget
+   2. Clone the recurrencelib.js and paste it as a subnote of recurrenceWidget.js
+   3. Create a note relation ~recurrenceTemplate= and point it to the template for recurring notes created in step 1
+4. Create a JS Frontend script and copy the contents of [markDone.js](./markDone.js).
+   1. Clone the recurrencelib.js and paste it as a subnote of markDone.js
+   2. Create a label of #dueDateLabel= where dueDate is the datetime label created for the template on step 1
+5. Create a JS frontend script and copy the contents of [setupButton.js](./setupButton.js).
+   1. Set the label "#run=frontendStartup" as a label for setupButtons.js
+   2. Create a note relation ~markDoneScript= pointing to the markDone.js note
+6.  Reload the frontend or restart trilium
+7. Use the newly created launcher to mark the task as done and update the recurrence of a given note
+
+
+## Changelog
+
+### 2.0 
+- Switch to labels and relations instead of in code variables
+- Refactor Code
+
+### 1.0 
+- Initial Release
+
+
---
+++ Recurrence/markDone.js
@@ -0,0 +1,43 @@
+/*
+    Instructions: Paste the below into a new JS Backend Script note. 
+    Clone the requirrencelib.js widget to to this script as a subnote
+    Create a label "#dueDateLabel=" with the label of notes with due dates to be repeated
+*/
+
+// User Editable ---------------------------------------------------------------------------
+let recurrencelib = require("recurrencelib")
+
+async function run_script() {
+    const dueLabel = await api.currentNote.getLabelValue("dueDateLabel")    
+    var currentNote = await api.getActiveContextNote();
+    if (currentNote.hasLabel(dueLabel)) {
+        const recurrenceJSON = await api.runOnBackend(
+            (currentNoteID) => {
+                const note = api.getNote(currentNoteID)
+                const attachment = note.getAttachmentByTitle("Recurrence.json")
+                return attachment ? attachment.getContent().toString() : ""
+                
+        }, [currentNote.noteId])
+        if (recurrenceJSON){
+            var due = new Date(currentNote.getLabelValue(dueLabel))
+            const recurrence = recurrencelib.recurrenceFromJSON(recurrenceJSON)
+            const newDue = recurrence.getNextDate(due)
+            const year = String(newDue.getFullYear())
+            const month = String(newDue.getMonth()+1).padStart(2, '0')
+            const day = String(newDue.getDate()).padStart(2, '0')
+            const hour = String(newDue.getHours()).padStart(2, '0')
+            const minute = String(newDue.getMinutes()).padStart(2, '0')
+            const newDueString = `${year}-${month}-${day}T${hour}:${minute}`
+            await api.runOnBackend((currentNoteID, dueLabel, newDue) => {
+                const currentNote = api.getNote(currentNoteID)
+                currentNote.setLabel(dueLabel, newDue)
+            }, [currentNote.noteId, dueLabel, newDueString]);    
+
+        } else {
+            await currentNote.setLabel(dueLabel, "")
+            await currentNote.setLabel("archived")
+        }
+        api.refreshIncludedNote(currentNote) 
+    }
+}
+run_script()
