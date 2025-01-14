cd59785238176fc9a7584ef9079bfe34ec2da4cd
beatlink <beatlink>
2025-01-13 21:16:41 -0500

    Update Agenda

--- Agenda/README.md
+++ Agenda/README.md
@@ -4,7 +4,24 @@ Agenda Script for Trilium
 
 ## Overview
 
-These scripts allow you to sort your to-dos into various notes depending on their due dates:
+These scripts allow you to organize your to-dos 
+
+First agenda searches for notes matching specific search criteria. Any search criteria you can type into the search bar, you can load into a profile
+Next, agenda can prefix and filter by their due date labels, number labels or text labels. 
+Finally, agenda can set specific prefixes of the notes based on the value of their labels. 
+
+## Profiles
+
+Agenda operates using JSON profiles storing the search criteria, prefix type and prefix rules. You can therefore create profiles for example
+
+- Work Tasks
+- Home Tasks
+- Repeating Tasks
+- Projects
+- Future Tasks
+
+### Date Profile
+The date profile allows you to sort your todos by due date into various intervals. The [project-date](./profiles/project-date.json) and [routines-date](./profiles/project-date.json) profiles organize tasks by the following criteria. 
 
 - Overdue
 - Today
@@ -13,20 +30,96 @@ These scripts allow you to sort your to-dos into various notes depending on thei
 - This Year
 - Future
 
-## To Use
-1. Create 6 notes to store tasks for the above categories.
+
+### Number Profile
+The number profile allows you to prefix your todos with a numeric value. This is used by the [project-number](./profiles/project-number.json) profile. 
+
+
+## JSON Structure
+
+
+### Profile Name
+All profiles start with a top level object giving the name of a profile. This allows you to store multiple profiles in a single json note though it is recommended to store each profile in its own note. This profile name must be unique.
+
+### Search Criteria
+This is the search terms that selects the notes that will be processed by this profile. Anything that you can type into Trilium's search bar can be used here to filter your notes. 
+
+### ParentNoteID
+All notes matching the search criteria will be added as a child of this note.
+
+### Type
+The profile type determines how tasks are prefixed and filtered. Valid values are 'date', 'number' and 'text'
+
+### DateLabel (type = "date")
+This is only used in 'date' profile type. Notes found by the searchCriteria are prefixed according to this label that should be on all matching notes. 
+
+### UseNumberOfDays (type = "date")
+If set to true, intervals such as weeks, months and years will be sorted by the number of days away from the present date. For example, if an interval is configured for endOfThisWeek, it will show tasks 7 days from now, rather than the next actual week
+
+### EnableNoDueDate (type = "date")
+If true, tasks with no due date set will still be shown under the parent ID. If false, such tasks would not be added at all.
+
+### Intervals (type = "date")
+This is a list of interval objects that tasks will be sorted under. Each interval can have a custom date range, prefix and filter to determine whether it is shown or not
+
+### Interval Name (type = "date")
+Every interval must have a unique name. Intervals with the same name will result in undefined behavior
+
+### DateCriteria (type = "date")
+The date criteria determines the date range that corresponds to this interval. The first element of the list corresponds to the day.js function that is called to check whether a task falls in this interval range. The remainder of the list consist of the arguments that are passed to the day.js function. 
+
+The following placeholder variables will be replaced by the actual date values in the day.js function and are self explanatory.
+* now
+* startOfToday
+* endOfToday
+* endOfThisWeek
+* endOfThisMonth
+* endOfThisYear
+
+### FormatString (type = "date")
+This string is passed to the day.js format function to get the string format of the date that will be used as the prefix. To configure date formats see https://day.js.org/docs/en/display/format
+
+### HideCriteria (type = "date")
+If the parent note has a label and value matching this criteria, that specific interval will be hidden. This allows you to for example create a checkbox label on the parent note to show or hide tasks due next week. 
+
+The first element of the list is the parent's name and the second is its value. 
+
+### NumberLabel (type = "number")
+This is only used for the 'number' profile type. The value of this label will be used to prefix your matching notes notes
+
+### EnableNoNumber (type = "number")
+If set to true, this will add matching tasks without a number label value to the parent note. If false, such tasks would be filtered out. 
+
+### TextLabel (type = "text")
+This is only used for the 'text' profile type. The value of this label will be used to prefix your notes according to the PrefixMap described below
+
+### enableNoValue (type = "text")
+If set to true, this will add matching tasks without a text label value to the parent note. If false, such tasks would be filtered out. 
+
+### PrefixMap (type = "text")
+This object converts the value of the text label to the prefix that should be used. That is, a task with a textLabel value of the key will be given a prefix of its associated value. 
+
+## Quick Setup
+1. Create a parent note to store the tasks processed by a specific profile.
 2. Create a JS backend script and copy the contents of [updateAgenda.js](./updateAgenda.js)
-   1. Set "#run=hourly" as a label for the updateAgenda.js script
-   2. Set the note IDs for the 6 notes you created in the respective variables under "User Set Variables"
-   3. Also set the dueTime label and dueDate label if they are different from the default
+   a. Set "#run=hourly" as a label for the updateAgenda.js script
 3. Create a JS frontend script and copy the contents of [setupButton.js](./setupButton.js). 
-   1. Set "#run=frontendStartup" as a label for this script.
+   a. Set "#run=frontendStartup" as a label for this script.
+4. Create a JSON note for the profile you wish to use
+   a. You can copy and use a profile from the [profiles](./profiles/) folder of this repository. 
+   b. Set the search criteria to the criteria matching your notes
+   c. Set the parentNoteID to the id of the parent note you created in step 1. 
+5. Create an '~agendaProfile' relationship on your updateAgenda.js note created in step 2 and set the value of this relationship to the JSON profile note you created in step 4.
 4. Reload the frontend or restart Trillium
-5. For notes you want to track, give them a dueDate label of type date.
-6.  Use the newly created launcher or wait an hour for the automatic update to update agenda.
+5.  Use the newly created launcher or wait an hour for the automatic update to update agenda.
 
 
 ## Changelog
+
+### 3.0 
+- Migration to Profile System
+   Agenda now uses separate JSON profiles for different task types. 
+- Addition of number and text type to the date type. 
 
 ### 2.0
 - Added option to use number of days for intervals
--- Agenda/updateAgenda.js
+++ Agenda/updateAgenda.js
@@ -1,108 +1,121 @@
 /*
     Instructions: Paste the below into a new JS Backend Script note. 
-    Set the note variables to the IDs of the notes you want to use for your categories.
+    Set ~agendaProfile relationships pointing to the JSON profile notes of the profiles you wish to load.
     Remember to set #run=hourly as a label for this note.
-    To configure date formats use https://day.js.org/docs/en/display/format
 */
 
 async function run_script() {
-    // Imports -----------------------------------------------------------------------------
-    const isBetween = (await import('dayjs/plugin/isBetween.js')).default
+    
+    // Imports
+    const isBetween = (await import("dayjs/plugin/isBetween.js")).default
     api.dayjs.extend(isBetween)
 
-    const advancedFormat = (await import('dayjs/plugin/advancedFormat.js')).default
+    const advancedFormat = (await import("dayjs/plugin/advancedFormat.js")).default
     api.dayjs.extend(advancedFormat)
 
-    const isSameOrBefore = (await import('dayjs/plugin/isSameOrBefore.js')).default
+    const isSameOrBefore = (await import("dayjs/plugin/isSameOrBefore.js")).default
     api.dayjs.extend(isSameOrBefore)
 
-    // Variables --------------------------------------------------------------------
-    let useNumberOfDays = true // intervals are determined based on number of days
-    let now = api.dayjs()
-    let startOfToday = now.startOf("day")
-    let endOfToday = now.endOf("day")
-    let endOfThisWeek = useNumberOfDays ? startOfToday.add(7, "day") : now.endOf("week")
-    let endOfThisMonth = useNumberOfDays ? startOfToday.add(30, "day") : now.endOf("month")
-    let endOfThisYear = useNumberOfDays ? startOfToday.add(365, "day") : now.endOf("year")
-
-    // User Set Variables -----------------------------------------------------------------
-    let dueDatetimeLabel = "dueDate"
-    let searchCriteria = `# ~template.title="Routine" OR ~template.title="Project"`
-    let parentNotes = {
-        "now": "zUgTNjqLMROR",
-        "upcoming": "SpWaU2490gYz"
+    // Load Profiles
+	let allProfiles = {}
+    let profileRelations = api.currentNote.getRelations("agendaProfile")
+    for (let relation of profileRelations) {
+        let profileNote = api.getNote(relation.value)
+    	var profiles = JSON.parse(profileNote.getContent())
+        allProfiles = {...allProfiles, ...profiles}
     }
-    let intervals = {
-        'overdue': {
-            'criteria': (datetime) => {
-                return datetime.isBefore(startOfToday)
-            },
-            'parent': 'now',
-            'formatString': "MMM D, YYYY HH:mm"
-        },
-        'now': {
-            'criteria': (datetime) => {
-                return datetime.isBefore(now)
-            },
-            'parent': 'now',
-            'formatString': "HH:mm"
-        },
-        'restOfDay': {
-            'criteria': (datetime) => {
-                return datetime.isBetween(now, endOfToday, null, '[)')
-            },
-            'parent': 'upcoming',
-            'formatString': "HH:mm"
-        },
-        'thisWeek': {
-            'criteria': (datetime) => {
-                return datetime.isBetween(endOfToday, endOfThisWeek, null, '[)')
-            },
-            'parent': 'upcoming',
-            'formatString': "ddd"
-        },
-        'thisMonth': {
-            'criteria': (datetime) => {
-                return datetime.isBetween(endOfThisWeek, endOfThisMonth, null, '[)')
-            },
-            'parent': 'upcoming',
-            'formatString': "Do"
-        },
-        'thisYear': {
-            'criteria': (datetime) => {
-                return datetime.isBetween(endOfThisMonth, endOfThisYear, null, '[]')
-            },
-            'parent': 'upcoming',
-            'formatString': "MMMM"
-        },
-        'future': {
-            'criteria':  (datetime) => {
-                return datetime.isAfter(endOfThisYear)
-            },
-            'parent': 'upcoming',
-            'formatString': "YYYY"
+
+    // Get Parent Notes
+    let parentNoteIDs = new Set()
+    for (const [name, profile] of Object.entries(allProfiles)) {
+        parentNoteIDs.add(profile["parentNoteID"])
+    }
+
+    //Delete Notes Under Parents
+    for (let parentNoteID of parentNoteIDs){
+        let parentNote = api.getNote(parentNoteID)
+        for (let note of await parentNote.getChildNotes()){
+            api.toggleNoteInParent(false, note.noteId, parentNoteID)
         }
     }
-    // Clear existing note branches
-    for (parent in parentNotes){
-        var parentNote = parentNotes[parent]
-        for (let note of api.getNote(parentNote).getChildNotes()){
-            api.toggleNoteInParent(false, note.noteId, parentNote)
-        } 
-    }
-    // Set notes according to criteria
-    for (let note of api.searchForNotes(searchCriteria)){
-        if (note.getLabelValue(dueDatetimeLabel)){
-            let dueDatetime = api.dayjs(note.getLabelValue(dueDatetimeLabel))       
-            for (let interval in intervals){
-                var criteria = intervals[interval]['criteria']
-                var parent = parentNotes[intervals[interval]['parent']]
-                var formatString = intervals[interval]['formatString']
-                var datetimeFormatted = dueDatetime.format(formatString)
-                if (criteria(dueDatetime)){
-                    api.toggleNoteInParent(criteria, note.noteId, parent, datetimeFormatted)
-                    continue
-                }        
+
+    // Organize Notes According to Critera
+    let now = api.dayjs()
+    let startOfToday = now.startOf("day")
+    for (const [name, profile] of Object.entries(allProfiles)) {
+        let parentNote = api.getNote(profile["parentNoteID"])
+    	let allNotes = api.searchForNotes(profile["searchCriteria"])    
+        if (profile["type"] == "date") {
+            let dateLabel = profile["dateLabel"]
+            let useNumberOfDays = profile["useNumberOfDays"]
+            let dateVars = {
+                "now": now,
+                "startOfToday": startOfToday,
+                "endOfToday": now.endOf("day"),
+                "endOfThisWeek": useNumberOfDays ? startOfToday.add(7, "day") : now.endOf("week"),
+                "endOfThisMonth": useNumberOfDays ? startOfToday.add(30, "day") : now.endOf("month"),
+                "endOfThisYear": useNumberOfDays ? startOfToday.add(365, "day") : now.endOf("year"),
+            }
+            for (const [name, interval] of Object.entries(profile['intervals'])) {
+                // Skip this interval if hide criteria is met
+                if ("hideCriteria" in interval) {
+                    let hideLabel = interval["hideCriteria"][0]
+                    let hideValue = interval["hideCriteria"][1]
+                    if (parentNote.getLabelValue(hideLabel) == hideValue) {
+                        continue
+                    }
+                }
+                // Get the datetime function 
+                let dateFunc = interval["dateCriteria"].splice(0, 1)[0]
+
+                // Replace all variables in the function args
+                let args = interval["dateCriteria"]
+                for (let index in args){
+                    if (args[index] in dateVars) {
+                        args[index] = dateVars[args[index]]
+                    }
+                }
+                // Process the found notes
+                for (let note of allNotes){
+                    let noteDate = note.getLabelValue(dateLabel)
+                    if (noteDate){
+                        let date = api.dayjs(noteDate)                   
+                        if (date[dateFunc](...args)) {
+                            let prefix = date.format(interval["formatString"])
+                            api.toggleNoteInParent(true, note.noteId, parentNote.noteId, prefix) 
+                        }
+                    } else {
+                        // Add note to parent with "No Due Date" if enableNoDueDate
+                        if (profile["enableNoDueDate"]){
+                           api.toggleNoteInParent(true, note.noteId, parentNote.noteId, "") 
+                        }
+                    }
+                }
+        	}
+    	} else if (profile["type"] == "number") {
+            let numberLabel = profile["numberLabel"]
+            for (let note of allNotes){
+                let noteNumber = note.getLabelValue(numberLabel)
+                if (noteNumber){
+                    api.toggleNoteInParent(true, note.noteId, parentNote.noteId, String(noteNumber)) 
+                } else if (profile["enableNoNumber"]){
+                    api.toggleNoteInParent(true, note.noteId, parentNote.noteId, "") 
+                }
+            }
+        } else if (profile["type"] == "text") {
+            let textLabel = profile["textLabel"]
+            for (let note of allNotes){
+                let noteText = note.getLabelValue(textLabel)
+                if (noteText){
+                    if (profile["prefixMap"]){
+                        api.toggleNoteInParent(true, note.noteId, parentNote.noteId, String(profile["prefixMap"][noteText])) 
+                    } else {
+                        api.toggleNoteInParent(true, note.noteId, parentNote.noteId, String(noteText)) 
+
+                    }
+                } else if (profile["enableNoValue"]){
+                    api.toggleNoteInParent(true, note.noteId, parentNote.noteId, "") 
+                }
             }
         }
     }
