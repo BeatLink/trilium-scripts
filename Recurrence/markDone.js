0eb0fc29f1b8b2f340c39426036596dac161450b
beatlink <beatlink>
2025-05-22 20:34:56 -0500

    Update Recurrence

--- Recurrence/markDone.js
+++ Recurrence/markDone.js
@@ -1,46 +1,51 @@
 /*
-    Instructions: Paste the below into a new JS Backend Script note.
-    Clone the requirrencelib.js widget to to this script as a subnote
+    Instructions: Paste the below into a new JS Backend Script note. 
+    Create the rrule.min.js note as a subnote
     Create a label "#dueDateLabel=" with the label of notes with due dates to be repeated
 */
 
 // User Editable ---------------------------------------------------------------------------
-let recurrencelib = require("recurrencelib")
+let rrulelib = require("rrule.min.js")
+
+function formatDate(date) {
+  const pad = (n) => n.toString().padStart(2, '0');
+  return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}T${pad(date.getHours())}:${pad(date.getMinutes())}`;
+}
 
 async function run_script() {
     const dueLabel = await api.currentNote.getLabelValue("dueDateLabel")
-    var currentNote = await api.getActiveContextNote();
-    if (currentNote.hasLabel(dueLabel)) {
-        const recurrenceJSON = await api.runOnBackend(
-            (currentNoteID) => {
-                const note = api.getNote(currentNoteID)
-                const attachment = note.getAttachmentByTitle("Recurrence.json")
-                return attachment ? attachment.getContent().toString() : ""
-
+    const recurrenceLabel = await api.currentNote.getLabelValue("recurrenceLabel")
+    const currentNote = await api.getActiveContextNote();
+    if (currentNote.hasLabel(dueLabel) && currentNote.hasLabel(recurrenceLabel)) {
+        const recurrenceString = currentNote.getLabelValue(recurrenceLabel) 
+        const due = new Date(currentNote.getLabelValue(dueLabel))
+        var options = rrulelib.RRule.parseString(recurrenceString)
+		options.dtstart = due
+		var rrule = new rrulelib.RRule(options)        
+		const nextDate = rrule.after(due, false)
+        const nextDateString = formatDate(nextDate)
+        await api.runOnBackend((currentNoteID, dueLabel, newDue) => {
+            const currentNote = api.getNote(currentNoteID)
+            var content = currentNote.getContent()
+            content = content.replaceAll('checked="checked"', "")
+            currentNote.setContent(content, {forceSave: true})
+            currentNote.setLabel(dueLabel, newDue)
+            currentNote.removeLabel("archived") 
+            function unarchiveChildren(childNote){
+                childNote.removeLabel("archived")    
+                var children = childNote.getChildNotes()
+                for (let child of children){
+                    unarchiveChildren(child)
+                }
+            }
+            unarchiveChildren(currentNote)
+        }, [currentNote.noteId, dueLabel, nextDateString]);    
+    } else {
+        await api.runOnBackend((currentNoteID) => {
+            const currentNote = api.getNote(currentNoteID)
+        	currentNote.setLabel("archived")    
         }, [currentNote.noteId])
-        if (recurrenceJSON){
-            var due = new Date(currentNote.getLabelValue(dueLabel))
-            const recurrence = recurrencelib.recurrenceFromJSON(recurrenceJSON)
-            const newDue = recurrence.getNextDate(due)
-            const year = String(newDue.getFullYear())
-            const month = String(newDue.getMonth()+1).padStart(2, '0')
-            const day = String(newDue.getDate()).padStart(2, '0')
-            const hour = String(newDue.getHours()).padStart(2, '0')
-            const minute = String(newDue.getMinutes()).padStart(2, '0')
-            const newDueString = `${year}-${month}-${day}T${hour}:${minute}`
-            await api.runOnBackend((currentNoteID, dueLabel, newDue) => {
-                const currentNote = api.getNote(currentNoteID)
-                var content = currentNote.getContent()
-                content = content.replaceAll('checked="checked"', "")
-                currentNote.setContent(content, {forceSave: true})
-                currentNote.setLabel(dueLabel, newDue)
-            }, [currentNote.noteId, dueLabel, newDueString]);
-
-        } else {
-            await currentNote.setLabel(dueLabel, "")
-            await currentNote.setLabel("archived")
-        }
-        api.refreshIncludedNote(currentNote)
     }
+    api.refreshIncludedNote(currentNote) 
 }
 run_script()
