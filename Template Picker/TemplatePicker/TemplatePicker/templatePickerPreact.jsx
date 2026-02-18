783b3ef0ead713cd95f383d34c7f2839e7288045
BeatLink <git@beatlink.simplelogin.com>
2026-02-17 23:24:30 -0500

    Overhaul template picker

---
+++ Template Picker/TemplatePicker/TemplatePicker/templatePickerPreact.jsx
@@ -0,0 +1,50 @@
+import { defineWidget, useActiveNoteContext, useNoteProperty, RightPanelWidget, FormGroup, FormDropdownList, useEffect, useState } from "trilium:preact"
+import { searchForNotes, getActiveContextNote } from "trilium:api"
+
+export default defineWidget({
+    parent: "right-pane",
+    position: 1,
+    render() {
+        let defaultDropdownOption = [{ noteId: "none", name: "No Templates Found" }]
+        const [existingTemplates, setExistingTemplates] = useState(defaultDropdownOption)
+        const [dropdownValue, setDropdownValue] = useState("none")
+        const { note } = useActiveNoteContext()
+        const noteId = useNoteProperty(note, "noteId")
+        useEffect(() => {
+            (async () => {
+                setExistingTemplates(
+                    (await searchForNotes("#template #!noTemplatePicker orderBy note.title"))
+                        .map(note => ({ noteId: note.noteId, title: note.title }))
+                        .concat({ noteId: "none", title: "None" })
+                )
+                setDropdownValue(
+                    (await getActiveContextNote())
+                        .getRelationValue("template") ?? "none"
+                )
+            })()
+        }, [noteId])
+        const saveTemplate = (template) => {
+            api.runOnBackend((noteId, template) => {
+                if (template != "none") {
+                    api.getNote(noteId).setRelation("template", template)
+                } else {
+                    api.getNote(noteId).removeRelation("template")
+                }
+            }, [note.noteId, template])
+            setDropdownValue(template)
+        }
+        return (
+            <RightPanelWidget id="x-template-picker" title="Template">
+                <div id="x-template-picker-widget">
+                    <FormDropdownList
+                        values={existingTemplates}
+                        currentValue={dropdownValue}
+                        onChange={value => { saveTemplate(value) }}
+                        keyProperty="noteId" titleProperty="title"
+                        className="form-control dropdown-component"
+                    />
+                </div>
+            </RightPanelWidget>
+        )
+    }
+})
