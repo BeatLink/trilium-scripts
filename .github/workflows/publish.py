4d768fdbd0c3ee91ad915197acddd22c87427c52
BeatLink <git@beatlink.simplelogin.com>
2026-02-18 00:59:36 -0500

    Rename Plugin to Addon

--- .github/workflows/publish.py
+++ .github/workflows/publish.py
@@ -8,29 +8,29 @@ def main():
     # Get Metadata files
     metadata_files = list(Path('.').glob('**/metadata.json'))
 
-    merged_metadata = {"plugins": {}}
+    merged_metadata = {"addons": {}}
     for metadata_file in metadata_files:
         metadata_path = metadata_file.resolve()
 
         # Load and merge metadata files
-        plugin_id = ""
+        addon_id = ""
         with metadata_path.open() as f:
             metadata = json.load(f)
-            plugin_id = metadata.get("id")
-            merged_metadata["plugins"][plugin_id] = metadata
+            addon_id = metadata.get("id")
+            merged_metadata["addons"][addon_id] = metadata
 
-        # Zip the plugin folder
-        plugin_path = [x for x in metadata_path.parent.iterdir() if x.is_dir()][0]
-        if not plugin_path:
-            print(f"No plugin folder found for {metadata_path}, skipping zip")
+        # Zip the addon folder
+        addon_path = [x for x in metadata_path.parent.iterdir() if x.is_dir()][0]
+        if not addon_path:
+            print(f"No addon folder found for {metadata_path}, skipping zip")
             continue            
         shutil.make_archive(
-            base_name=plugin_id,
+            base_name=addon_id,
             format='zip',
-            root_dir=plugin_path,
+            root_dir=addon_path,
             base_dir=(".")
         )
-        print(f"Created zip: {plugin_id}.zip")
+        print(f"Created zip: {addon_id}.zip")
 
     # Write merged metadata
     with open("metadata.json", "w") as f:
