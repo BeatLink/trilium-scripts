6242f5d13f99c4637c2d52ca203eb975a63e15c5
BeatLink <git@beatlink.simplelogin.com>
2026-02-22 12:22:52 -0500

    Fix Area Picker

--- Area Picker/area-picker@beatlink/area-picker@beatlink/areaPickerPreact.jsx
+++ Area Picker/area-picker@beatlink/area-picker@beatlink/areaPickerPreact.jsx
@@ -4,37 +4,37 @@
     Set a #priorityLabel= label for the label to store the priority of each note
 */
 
-import { defineWidget, useActiveNoteContext, useNoteProperty, RightPanelWidget, FormGroup, FormDropdownList, useEffect, useState } from "trilium:preact";
-import { searchForNotes, getActiveContextNote, currentNote, log } from "trilium:api";
+import { defineWidget, useActiveNoteContext, useNoteProperty, RightPanelWidget, FormGroup, FormDropdownList, useEffect, useState } from "trilium:preact"
+import { searchForNotes, getActiveContextNote, currentNote, log } from "trilium:api"
 
 export default defineWidget({
-    parent: "right-pane",    
+    parent: "right-pane",
     position: 2,
     render() {
-        let defaultDropdownOption = [{noteId: "none", name: "No Area Found"}]
-        const [visible, setVisible] = useState(false);        
-        const [existingAreas, setExistingAreas] = useState(defaultDropdownOption);
-        const [dropdownValue, setDropdownValue] = useState("none");        
-        const { note } = useActiveNoteContext();
-        const noteId = useNoteProperty(note, "noteId");
+        let defaultDropdownOption = [{ noteId: "none", name: "No Area Found" }]
+        const [visible, setVisible] = useState(false)
+        const [existingAreas, setExistingAreas] = useState(defaultDropdownOption)
+        const [dropdownValue, setDropdownValue] = useState("none")
+        const { note } = useActiveNoteContext()
+        const noteId = useNoteProperty(note, "noteId")
         useEffect(() => {
-            (async () => { 
-                let profileNote = await currentNote.getRelationTarget("profile")
+            (async () => {
+                let profileNote = await currentNote.getRelationTarget("AddonData:profile")
                 let profile = JSON.parse(await profileNote.getContent())['values']
                 setVisible(
                     (await getActiveContextNote())
-                    .getLabelValue("label:area") ? true : false
+                        .getLabelValue("label:area") ? true : false
                 )
                 setExistingAreas(
                     Object.entries(profile)
-                    .map(([key, value]) => ({label: key, title: value.title}))
+                        .map(([key, value]) => ({ label: key, title: value.title }))
                 )
                 setDropdownValue(
                     (await getActiveContextNote())
-                    .getLabelValue("area") ?? "none" 
+                        .getLabelValue("area") ?? "none"
                 )
             })()
-        }, [noteId]);
+        }, [noteId])
         const saveArea = (area) => {
             api.runOnBackend((noteId, area) => {
                 let profileNote = api.currentNote.getRelationTarget("profile")
@@ -46,27 +46,27 @@ export default defineWidget({
                     api.getNote(noteId).removeLabel("area")
                     api.getNote(noteId).removeLabel("color")
                 }
-            }, [note.noteId, area]) 
-            setDropdownValue(area)     
+            }, [note.noteId, area])
+            setDropdownValue(area)
         }
         return (
             <>
-            {
-                visible && 
-                <RightPanelWidget id="x-area-picker" title="Area">
-                    <div id="x-area-picker-widget">
-                        <FormDropdownList
-                            class="dropdown-component"
-                            values={existingAreas}
-                            currentValue={dropdownValue}
-                            onChange={value => { saveArea(value)}}
-                            keyProperty="label" titleProperty="title"
-                            class="form-control"
-                        />
-                    </div>
-                </RightPanelWidget>
-            }
+                {
+                    visible &&
+                    <RightPanelWidget id="x-area-picker" title="Area">
+                        <div id="x-area-picker-widget">
+                            <FormDropdownList
+                                class="dropdown-component"
+                                values={existingAreas}
+                                currentValue={dropdownValue}
+                                onChange={value => { saveArea(value) }}
+                                keyProperty="label" titleProperty="title"
+                                class="form-control"
+                            />
+                        </div>
+                    </RightPanelWidget>
+                }
             </>
-        ) 
+        )
     }
 })
