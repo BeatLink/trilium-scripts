d42c60668ea441f2c8f9d23dc77c37b1097448f7
BeatLink <git@beatlink.simplelogin.com>
2026-07-02 17:01:52 -0500

    Update Expanded

---
+++ Expanded/Expanded/Expanded.js
@@ -0,0 +1,17 @@
+/*
+    Place the following code in a JS Backend Note. Create a relation "~runOnBranchChange" on the root node pointing to this script 
+*/
+
+
+async function expand(){
+    var notes = await api.searchForNotes('#alwaysExpanded');
+    for (var note of notes) {
+        for (var branch of await note.getParentBranches()) {
+            if (!branch.isExpanded){
+                branch.isExpanded = true;
+                branch.save();
+            }
+        }
+    }
+}
+expand()
