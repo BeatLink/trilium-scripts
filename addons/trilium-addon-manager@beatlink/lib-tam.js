eb9fa3de819215b8182177b00346252221e24a57
BeatLink <git@beatlink.simplelogin.com>
2026-07-07 15:57:58 -0500

    Update TAM

--- addons/trilium-addon-manager@beatlink/lib-tam.js
+++ addons/trilium-addon-manager@beatlink/lib-tam.js
@@ -12,7 +12,7 @@ const addonRootLabel = "addonRoot"
 const addonPersistenceLabel = "addonPersistence"
 const tamFileIdLabel = "TAMFILEID"
 const TAM_ID = "trilium-addon-manager@beatlink"
-const TAM_VERSION = "4.7.0"
+const TAM_VERSION = "4.7.1"
 const addonLabels = [
     "widget",
     "renderNote",
