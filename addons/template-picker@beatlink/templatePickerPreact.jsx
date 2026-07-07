192886fe89965b2dae736340b6e30507eee881d9
BeatLink <git@beatlink.simplelogin.com>
2026-07-07 14:20:09 -0500

    Whitespace fixing

--- addons/template-picker@beatlink/templatePickerPreact.jsx
+++ addons/template-picker@beatlink/templatePickerPreact.jsx
@@ -1,28 +1,28 @@
-import { defineWidget, useActiveNoteContext, useNoteProperty, RightPanelWidget, FormGroup, FormDropdownList, useEffect, useState } from "trilium:preact";
-import { searchForNotes, getActiveContextNote } from "trilium:api";
+import { defineWidget, useActiveNoteContext, useNoteProperty, RightPanelWidget, FormGroup, FormDropdownList, useEffect, useState } from "trilium:preact"
+import { searchForNotes, getActiveContextNote } from "trilium:api"
 
 export default defineWidget({
-    parent: "right-pane",    
+    parent: "right-pane",
     position: 1,
     render() {
-        let defaultDropdownOption = [{noteId: "none", name: "No Templates Found"}]
-        const [existingTemplates, setExistingTemplates] = useState(defaultDropdownOption);
-        const [dropdownValue, setDropdownValue] = useState("none");        
-        const { note } = useActiveNoteContext();
-        const noteId = useNoteProperty(note, "noteId");
+        let defaultDropdownOption = [{ noteId: "none", name: "No Templates Found" }]
+        const [existingTemplates, setExistingTemplates] = useState(defaultDropdownOption)
+        const [dropdownValue, setDropdownValue] = useState("none")
+        const { note } = useActiveNoteContext()
+        const noteId = useNoteProperty(note, "noteId")
         useEffect(() => {
-            (async () => { 
+            (async () => {
                 setExistingTemplates(
                     (await searchForNotes("#template #!noTemplatePicker orderBy note.title"))
-                    .map(note => ({noteId: note.noteId, title: note.title}))
-                    .concat({noteId: "none", title: "None"})
+                        .map(note => ({ noteId: note.noteId, title: note.title }))
+                        .concat({ noteId: "none", title: "None" })
                 )
                 setDropdownValue(
                     (await getActiveContextNote())
-                    .getRelationValue("template") ?? "none" 
+                        .getRelationValue("template") ?? "none"
                 )
             })()
-        }, [noteId]);
+        }, [noteId])
         const saveTemplate = (template) => {
             api.runOnBackend((noteId, template) => {
                 if (template != "none") {
@@ -30,22 +30,22 @@ export default defineWidget({
                 } else {
                     api.getNote(noteId).removeRelation("template")
                 }
-            }, [note.noteId, template]) 
-            setDropdownValue(template)     
+            }, [note.noteId, template])
+            setDropdownValue(template)
         }
         return (
             <RightPanelWidget id="x-template-picker" title="Template">
                 <div id="x-template-picker-widget">
-                        <FormDropdownList
-                            class="dropdown-component"
-                            values={existingTemplates}
-                            currentValue={dropdownValue}
-                            onChange={value => { saveTemplate(value)}}
-                            keyProperty="noteId" titleProperty="title"
-                            class="form-control"
-                        />
+                    <FormDropdownList
+                        class="dropdown-component"
+                        values={existingTemplates}
+                        currentValue={dropdownValue}
+                        onChange={value => { saveTemplate(value) }}
+                        keyProperty="noteId" titleProperty="title"
+                        class="form-control"
+                    />
                 </div>
             </RightPanelWidget>
-        )        
+        )
     }
 })
