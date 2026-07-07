97f58208f66a3fcf9acbb7ec5e240f4074c08c77
BeatLink <git@beatlink.simplelogin.com>
2026-07-07 16:49:40 -0500

    Update Agenda

--- addons/agenda@beatlink/agendaTask.jsx
+++ addons/agenda@beatlink/agendaTask.jsx
@@ -12,11 +12,12 @@ import { startNote } from "trilium:api"
 
 import { DatesDurationPicker } from "DatesDurationPicker.jsx"
 import { RecurrencePicker } from "RecurrencePicker.jsx"
-import { ActionPicker } from "ActionPicker.jsx"
+import { ActionBar } from "ActionBar.jsx"
 import { RankPicker } from "RankPicker.jsx"
 import { getAgendaSettings } from "agendaSettings.jsx"
 
 const { updateTaskLists } = require("libAgendaOverview.js")
+const { complete, rescheduleByDays } = require("libAgendaTask.js")
 
 // Main Widget ---------------------------------------------------------------------------
 function MainWidget(){
@@ -47,6 +48,27 @@ function MainWidget(){
         await updateTaskLists(ids.profileNoteIds, ids.constants, ids.icalNoteId)
     }
 
+    const actions = [
+        {
+            key: "complete",
+            icon: "bx bx-check",
+            text: "Complete Task",
+            onClick: async () => { await complete(noteId, ids.constants); await afterChange() }
+        },
+        {
+            key: "today",
+            icon: "bx bx-rocket",
+            text: "Start Today",
+            onClick: async () => { await rescheduleByDays(noteId, ids.constants, 0); await afterChange() }
+        },
+        {
+            key: "tomorrow",
+            icon: "bx bx-rocket",
+            text: "Start Tomorrow",
+            onClick: async () => { await rescheduleByDays(noteId, ids.constants, 1); await afterChange() }
+        }
+    ]
+
     return (
         <RightPanelWidget title="Task">
             <div className="agenda-widget">
@@ -60,7 +82,7 @@ function MainWidget(){
                 </div>
                 <div>
                     <label>Actions</label>
-                    <ActionPicker constants={ids.constants} onAfterChange={afterChange}/>
+                    <ActionBar actions={actions}/>
                 </div>
                 <div>
                     <label>Rank</label>
