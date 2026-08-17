f9140cf83730c16b37967dc6a12f747445230b6c
BeatLink <git@beatlink.simplelogin.com>
2026-08-17 12:01:07 -0500

    chore(simplecalendar): migrate to schema-driven settings and TAM's synthesized root

--- addons/simplecalendar@beatlink/view.jsx
+++ addons/simplecalendar@beatlink/view.jsx
@@ -9,7 +9,7 @@ export default function SimpleCalendarView() {
         (async () => {
             const schemaNoteId = await api.currentNote.getRelationValue("schemaNote")
             const settingsNoteId = await api.currentNote.getRelationValue("settingsNote")
-            const configNoteId = (await api.getNote(settingsNoteId)).getRelationValue("AddonData:config")
+            const configNoteId = (await api.getNote(settingsNoteId)).getRelationValue("configNote")
             setSettings(await loadSettings(schemaNoteId, configNoteId))
         })()
     }, [])
